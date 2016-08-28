d34b1770eb586820df14c9baadea98abdfa6c1d1
sajan <sajanpoddar98@gmail.com>
2016-08-29 03:28:33 +0600

    try to stripe chargeing credit card

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,8 @@ use App\Http\Requests;
 use App\Product;
 use App\Cart;
 use Session;
+use Stripe\Charge;
+use Stripe\Stripe;
 
 class ProductController extends Controller
 {
@@ -47,5 +49,27 @@ class ProductController extends Controller
     	$cart = new Cart($oldCart); 
     	$total=$cart->totalPrice;
     	return view('shop.checkout',['total' => $total]);
+    }
+    public function postCheckout(Request $request){
+            if(!Session::has('cart')){
+            return redirect()->route('shop.shoppingCart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart); 
+
+        Stripe::setApiKey('sk_test_aiAkESC2CKfIs1fcLVTWKBiD');
+        try{
+            Charge::create(array(
+              "amount" => $cart->totalPrice * 100,
+              "currency" => "usd",
+              "source" => $request->input('stripeToken'), // obtained with Stripe.js
+              "description" => "Test Charge"
+            ));
+        }
+        catch (\Exception $e){
+            return redirect()->route('checkout')->with('error', $e->getMessage());
+        }
+        Session::forget('cart');
+        return redirect()->route('product.index')->with('success' , 'successfully purchased products');
     }
 }
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,6 +5,15 @@ Laravel Shopping cart
 @endsection
 
 @section('content')
+@if(Session::has('success'))
+<div class="row">
+      <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+      <div id="charge-message" class="alert alert-success">
+        {{ Session::get('success')  }}
+      </div>
+</div>
+</div>
+@endif
 @foreach($products->chunk(3) as $productchunk)
  <div class="row">
  @foreach($productchunk as $product)
